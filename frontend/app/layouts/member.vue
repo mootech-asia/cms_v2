3b778ee3882ed5f9f84abb5c0a2990aa99ad8c8d
JKson <jkson.min@mootech.asia>
2026-07-16 23:01:27 +0800

    fix: offset member content for fixed navigation

--- frontend/app/layouts/member.vue
+++ frontend/app/layouts/member.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="min-h-screen overflow-x-hidden bg-surface-deep">
     <MemberHeader />
-    <div class="flex min-h-screen">
+    <div class="min-h-[calc(100vh-4rem)]">
       <MemberSidebar />
-      <main class="min-w-0 flex-1 p-4 pb-24 md:p-8 md:pb-24">
+      <main class="min-w-0 p-4 pb-24 md:ml-64 md:p-8 md:pb-24">
         <slot />
       </main>
     </div>
